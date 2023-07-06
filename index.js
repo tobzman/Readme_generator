688a0bc4ce41dea60b4b98ce1d9c0fa7062f7eaf
Tobenna <tvokafor@gmail.com>
2023-07-06 11:40:26 +0100

    Updated files

--- index.js
+++ index.js
@@ -1,68 +1,62 @@
 const inquirer = require("inquirer");
-const generateMarkdown = require("utils/generateMarkdown.js")
- const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 inquirer
   .prompt([
     {
       type: "input",
       name: "projectTitle",
       message: "Enter the title of your project:",
-      
     },
     {
       type: "input",
-      name: "ProjectDescription",
+      name: "projectDescription",
       message: "Enter a Description for your Project:",
-      
     },
     {
       type: "input",
       name: "installation",
       message: "Enter installation instructions:",
-     
     },
     {
       type: "input",
       name: "usage",
       message: "Enter usage information:",
-     
     },
     {
       type: "input",
       name: "contribution",
       message: "Enter contribution guidelines:",
-     
     },
     {
-      type: 'input',
-      name: 'tests',
-      message: 'Enter test instructions:',
+      type: "input",
+      name: "tests",
+      message: "Enter test instructions:",
     },
     {
-      type: 'list',
-      name: 'license',
-      message: 'Choose a license for your project:',
-      choices: ['MIT', 'Apache 2.0', 'GNU GPLv3'],
+      type: "list",
+      name: "license",
+      message: "Choose a license for your project:",
+      choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
     },
     {
-      type: 'input',
-      name: 'githubUsername',
-      message: 'Enter your GitHub username:',
+      type: "input",
+      name: "githubUsername",
+      message: "Enter your GitHub username:",
     },
     {
-      type: 'input',
-      name: 'email',
-      message: 'Enter your email address:',
+      type: "input",
+      name: "email",
+      message: "Enter your email address:",
     },
-
   ])
   .then((answers) => {
     const readmeContent = generateMarkdown(answers);
-    fs.writeFile('README.md', readmeContent, (err) => {
+    fs.writeFile("README.md", readmeContent, (err) => {
       if (err) {
         console.error(err);
       } else {
-        console.log('README.md file generated successfully!');
+        console.log("README.md file generated successfully!");
       }
     });
-  );
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,33 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license) {
-    
+  if (license) {
     return `![License](https://img.shields.io/badge/license-MIT-blue.svg)`;
   }
-  return '';
+  return "";
 }
-
-
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {f (license) {
-    
-    return'';
+function renderLicenseLink(license) {
+  if (license) {
+    return "";
   }
-  
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {if (license) {
-    
-  return '';
-}}
+function renderLicenseSection(license) {
+  if (license) {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(answers) {
   return `# ${answers.projectTitle}
   ${answers.projectDescription}
   ${answers.installation}
